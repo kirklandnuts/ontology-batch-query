77ab0182003bf4ab25f0dbc35c0a21ab7be57925
timothy <nguyen.d.timothy@gmail.com>
2017-06-13 15:53:39 -0700

    command line usage works

--- README.md
+++ README.md
@@ -2,50 +2,124 @@
 This is a module for batch querying ontologies using [BioPortal's API](http://data.bioontology.org/documentation) to resolve a list of terms.
 
 ## Usage
+
+### Command Line functionality
+```
+$ python3 batch_query.py /Users/timothy/Documents/ontology-batch-query/examples/measurements/measurements.txt
+
+
+The terms in /Users/timothy/Documents/ontology-batch-query/examples/taxon/taxon.txt have been resolved through BioPortal.
+Results have been stored in /Users/timothy/Documents/ontology-batch-query/examples/taxon/resolved_terms.csv.
+```
+Both of these files are included (along with others) in the `examples` folder in this repository.
+
+
+### Importing functions for use in other script or in python console
 ```
 >>> from batch_query import *
 ```
 
-### Resolving a single term
+#### Resolving a single term
 ```
->>> melanoma_matches = resolve_term('melanoma')
+>>> melanoma_matches = resolve_term('melanoma', [])
 ```
-The variable `melanoma_matches` contains a list of dictionaries, with each dictionary representing an individual resolution through an ontology on BioPoral.
 
-To clarify, the variable `melanoma_matches` contains the list `collection` from within [the nested JSON object representing the response](http://data.bioontology.org/search?q=melanoma). NOTE: This function only captures non-naked terms (those with provided definitions). 
+The variable `melanoma_matches` contains a list of dictionaries, with each dictionary representing an individual resolution through an ontology on BioPortal. The BioPortal API limits each page response to containing no more than 50 matches. Resolve_term() handles the collection of the comprehensive set of matches with three lines of code (lines 79-81 in `batch_query.py`) that employ a while loop to iterate through all the available pages. Currently, I have commented out those three lines, limiting the amount of matches per searched term.
+
+```
+>>> len(melanoma_matches)
+50
+```
+
+To clarify, the variable `melanoma_matches` contains the list `collection` from within [the nested JSON object representing the response](http://data.bioontology.org/search?q=melanoma) with some additional information added to each element of the list (done in `resolve_term()`). NOTE: This function only captures non-naked terms (those with provided definitions).
 
 Accessing the definition from the first match in the list of matches:
 
 ```
 >>> melanoma_matches[0]['definition']
 ['A malignant neoplasm derived from cells that are capable of forming melanin, which may occur in the skin of any part of the body, in the eye, or, rarely, in the mucous membranes of the genitalia, anus, oral cavity, or other sites. It occurs mostly in adults and may originate de novo or from a pigmented nevus or malignant lentigo. Melanomas frequently metastasize widely, and the regional lymph nodes, liver, lungs, and brain are likely to be involved. The incidence of malignant skin melanomas is rising rapidly in all parts of the world. (Stedman, 25th ed; from Rook et al., Textbook of Dermatology, 4th ed, p2445)']
-
 ```
 
-### Resolving a list of terms
+#### Resolving a list of terms
 ```
 >>> terms = ['fish','bird','dog']
->>> resolved_terms = resolve_list(terms)
+>>> resolved_terms = resolve_list(terms, [])
 ```
-`resolve_list()` simply calls `resolve_term()` on each term in the list argument and returns them in a structured form. Here, `resolved_terms` contains a list of dictionaries, with each dictionary representing a searched term and all of its matches. Here it is visualized:
+`resolve_list()` simply calls `resolve_term()` on each term in the list argument and returns them in a structured form. Here, `resolved_terms` is a list of lists of dictionaries, with each nested list representing a searched term and with each dictionary within that list representing a single match. Here it is visualized:
 
 ```
 [
-	{
-		'term':'fish', 
-		'matches':<RETURNED VALUE FROM resolve_terms('fish')>
-	},
-	{
-		'term':'bird', 
-		'matches':<RETURNED VALUE FROM resolve_terms('bird')>
-	},
-	{
-		'term':'dog', 
-		'matches':<RETURNED VALUE FROM resolve_terms('dog')>
-	}
+	[
+		{<FIRST ELEMENT OF LIST RETURNED FROM resolve_term('fish')>}
+		{<SECOND ELEMENT OF LIST RETURNED FROM resolve_term('fish')>}
+		...
+		{<Kth ELEMENT OF LIST RETURNED FROM resolve_term('fish')>}
+	],
+	[
+		{<FIRST ELEMENT OF LIST RETURNED FROM resolve_term('bird')>}
+		{<SECOND ELEMENT OF LIST RETURNED FROM resolve_term('bird')>}
+		...
+		{<Mth ELEMENT OF LIST RETURNED FROM resolve_term('bird')>}
+	],
+	[
+		{<FIRST ELEMENT OF LIST RETURNED FROM resolve_term('dog')>}
+		{<SECOND ELEMENT OF LIST RETURNED FROM resolve_term('dog')>}
+		...
+		{<Nth ELEMENT OF LIST RETURNED FROM resolve_term('dog')>}
+	]
 ]
 ```
 
+where K, M, and N = the number of total matches returned by calling resolve_term() on 'fish', 'bird', and 'dog' respectively
+
+
+## Notes
+### Speed
+Time to resolve the 4 terms in `measurements.txt` (found in `examples` folder) when amount of matches is limited to 50:
+```
+$ time python3 batch_query.py /Users/timothy/Documents/ontology-batch-query/examples/measurements/measurements.txt
+
+
+The terms in /Users/timothy/Documents/ontology-batch-query/examples/measurements/measurements.txt have been resolved through BioPortal.
+Results have been stored in /Users/timothy/Documents/ontology-batch-query/examples/measurements/resolved_terms.csv.
+
+
+real	0m54.349s
+user	0m1.205s
+sys	0m0.202s
+```
+
+Here I do the same thing, this time without limiting the amount of matches per term, and had to interrupt the process before it could finish. I'm not sure how far it got, but it ran for a very long time before I stopped it:
+```
+$ time python3 batch_query.py /Users/timothy/Documents/ontology-batch-query/examples/measurements/measurements.txt
+^CTraceback (most recent call last):
+  File "batch_query.py", line 165, in <module>
+    output_batch_query(inputFile, outputDirectory)
+  File "batch_query.py", line 40, in output_batch_query
+    results_list = resolve_list(terms, ontologies_param) # list of lists each representing the matches of a term and
+  File "batch_query.py", line 63, in resolve_list
+    search_results.append(resolve_term(term, ontologies_param))
+  File "batch_query.py", line 102, in resolve_term
+    time.sleep(.1)
+KeyboardInterrupt
+
+real	37m16.905s
+user	0m12.848s
+sys	0m1.973s
+```
+
+
+### Other Features
+[BioPortal's API documentation](http://data.bioontology.org/documentation) shows its other capabilities. Because I am not 100% sure about our ultimate goal with this script, I don't know what other features I can implement involving its extra capabilities. Please let me know if there is anything else I can add to this script/module to make it more useful.
+
 ## To-do
-- output results to .csv (`output_batch_query()` and `__main__` functionality WIP)
+- extend ontology scope functionality to command line use
+	- also improve ontology scope flow such that user doesn't have to input empty list as argument to search among all ontologies
+- allow user inputted limit of matches per searched term
+- improve output file naming procedure
+	- currently outputs with the same filename every time (sorry)
 - consider unresolvable terms
+	- haven't run into one yet (haven't tried a wide variety of terms yet)
+- clean code
+	- standard naming convention
+	- handle key custom encoding within resolve_term()
--- batch_query.py
+++ batch_query.py
@@ -1,7 +1,8 @@
 ## -- HEADER --
 
 '''
-5/18/17
+Last updated on 6/13/17
+Created on 5/18/17
 Adapted by Timothy Nguyen, NCEAS from Bioportal API sample code:
     https://github.com/ncbo/ncbo_rest_sample_code/blob/master/python/classes_search.py
 '''
@@ -11,91 +12,104 @@ import json
 import csv
 import os
 import sys
+import time
+
+
+# -- GLOBAL VARIABLES --
 
 REST_URL = 'http://data.bioontology.org'
 API_KEY = 'a2580539-56ae-420f-b558-3ab624285dbe'
+STORED_ONTOLOGIES = {}
+MIN_KEYS = ['prefLabel', 'definition', 'synonym', '@id']
+ALL_KEYS = ['searched_term','prefLabel', 'definition', 'synonym', 'ontology_acronym', 'ontology_name', 'last_updated', 'has_child', '@id']
 
-
-## -- FUNCTIONS --
+# -- FUNCTIONS --
 
 # precondition: The first argument is a path to a text file that contains a list of terms to be searched separated
 #   by newline characters. The second argument is a path to a directory in which the csv containing the batch
-#   query results is to be outputted
+#   query results is to be outputted. The optional third argument is is a list of strings, each string being a
+#   (CASE SENSITIVE) acronym associated with an ontology. This list of acronyms is the subset of ontologies from which
+#   the function will return matches. By default, this function considers all ontologies.
 # postcondition: Outputs a csv file containing matches from ontologies on Bioportal for the searched terms
-def output_batch_query(inputPath, outputPath, ontologies):
+def output_batch_query(inputPath, outputPath, ontologies_param = None):
+    if ontologies_param is None:
+        ontologies_param = []
     # populating a list with terms from the input file
     terms_file = open(inputPath, 'r')
     terms = []
     for line in terms_file:
         terms.append(line.strip())
     # use resolve_list() to resolve list of terms
-    results_list = resolve_list(terms, ontologies)
+    results_list = resolve_list(terms, ontologies_param) # list of lists each representing the matches of a term and
+    #   containing dictionaries of which each represent a single match
     # creating output csv file
     outfile = outputPath + '/resolved_terms.csv'
     f = csv.writer(open(outfile, 'w'))
-    f.writerow(["searched_term", "matched_term", "definition", "synonyms", "URL"])
+    f.writerow(["searched_term", "matched_term", "definition", "synonyms", "ontology_acronym", "ontology_name",
+               "last_updated", "has_child","URL"])
     # populating csv with matches
-    # THIS WAS WRITTEN ASSUMING THAT ALL MATCHES HAD DEFINITIONS SO ITS BROKEN NOW BECAUSE MARK WANTS NAKED TERMS AS WELL
-    #   GOOD JOB MARK YOU BROKE MY FUNCTION >:-(
-    # for dict in results_list:
-    #     for match in dict['matches']:
-    #         try:
-    #             f.writerow(dict['term'], match['prefLabel'], match['definition'], match['synonym'], match['@id'])
-    #         except KeyError:
-    #             f.writerow(dict['term'], match['prefLabel'], match['definition'], '', match['@id'])
+    for term_matches in results_list: # iterating through list of lists; each iteration is list of all matches for a term
+        for match in term_matches: # iterating thruh list of dictionaries each representing a match
+            record = []
+            for key in ALL_KEYS:
+                record.append(match[key])
+            f.writerow(record)
 
-# precondition: the argument is a dictionary representing a single match of a term from an ontology
-# postcondition: the function will return True if the input match has at least one child term and False otherwise
-def hasChild(match):
-    children = get_json(match['links']['children'])
-    return(children['totalCount'] > 0)
 
-# precondition: the argument is a dictionary representing a single match of a term from an ontology
-# postcondition: the function will return a list in which the first element is the string representation of the acronym
-#   associated with the ontology from which the match was found and the second element is the date of the most recent
-#   update to the ontology
-def ontology_info(match):
-    ontology = get_json(match['links']['ontology'])
-    ontology_subs = get_json(ontology['links']['submissions'])
-    my_ontology = {'name' : ontology['acronym'],
-                   'date' : ontology_subs[0]['creationDate'][:10]}
-    return(my_ontology)
 
-# precondition: Argument is a list full of strings of terms to be resolved
-# postcondition: Returns a list of dictionaries each representing a searched term and its matches
-def resolve_list(listOfTerms, ontologies=None):
-    if ontologies is None:
-        ontologies = []
+# precondition: 1st argument is a list containing strings of terms to be resolved; The second argument is a list of
+#   strings, each string being a (CASE SENSITIVE) acronym associated with an ontology. This list of acronyms is the
+#   subset of ontologies from which the function will return matches.
+# postcondition: Returns a list of lists of dictionaries with each nested list representing a searched term and
+#   each dictionary within representing a match of the term
+def resolve_list(listOfTerms, ontologies_param):
     search_results = []
     for term in listOfTerms:
-        result_dict = {'term':term, 'matches':resolve_term(term, ontologies)}
-        search_results.append(result_dict)
+        search_results.append(resolve_term(term, ontologies_param))
     return search_results
+
 
 # precondition: The first argument is a string representation of the searched term. The second argument is a list of
 #   strings, each string being a (CASE SENSITIVE) acronym associated with an ontology. This list of acronyms is the
-#   subset of ontologies from which the function will return matches. By default, the function will consider all
-#   ontologies.
-# postcondition: Returns a list of dicts, each object representing a match from the query
-def resolve_term(term, ontologies=None):
-    if ontologies is None:
-        ontologies = []
+#   subset of ontologies from which the function will return matches.
+# postcondition: Returns a list of dicts, each dict representing a match from the query
+def resolve_term(term, ontologies_param):
     # generate query and use it to get the initial page of matches
-    query = query_string(term, ontologies)
-    page = get_json(REST_URL + query)
+    page = get_json(REST_URL + term_query_string(term, ontologies_param))
     # iterate through all pages, appending each one to a list of pages
     pages = []
     pages.append(page)
-    while (page['nextPage'] != None):
-        page = get_json(page['links']['nextPage'])
-        pages.append(page)
-    # extract matches from each page in list of pages and insert them into a list of matches
+    # COMMENT OUT THE NEXT 3 LINES TO LIMIT THE AMOUNT OF MATCHES TO 50 (max amount presented on the each page)
+    # while (page['nextPage'] != None):
+    #     page = get_json(page['links']['nextPage'])
+    #     pages.append(page)
+    # extract matches from each page in list of pages and inserts them into a list of matches, adding some additional
+    #   information to each match ('ontology_acronym', 'ontology_name', 'last_updated', and 'has_child')
     matches = []
     for page in pages:
         for match in page['collection']:
+            # handling missing values by checking against hard-coded list of minimum keys (global var: MIN_KEYS)
+            for key in MIN_KEYS:
+                if key not in list(match.keys()):
+                    match[key] = 'NA'
+            # adding additional information before inserting the match into a list of matches
+            # adding searched term
+            match['searched_term'] = term
+            # adding ontology info
+            ontology_acronym = match['links']['ontology'][39:]
+            if ontology_acronym not in STORED_ONTOLOGIES.keys():
+                STORED_ONTOLOGIES[ontology_acronym] = ontology_info(ontology_acronym)
+            ontology = STORED_ONTOLOGIES[ontology_acronym]
+            match['ontology_acronym'] = ontology_acronym
+            match['ontology_name'] = ontology['name']
+            match['last_updated'] = ontology['date']
+            # adding key 'has_child' with boolean value representing whether or not the matched class has at least 1 child
+            time.sleep(.1) # to avoid overloading BioPortal's API; .1s is as low as I've set the delay so there is room for experimentation
+            match['has_child'] = get_json(match['links']['children'])['totalCount'] > 0
+
+            # inserting the newly completed match into the list of matches
             matches.append(match)
-    # grab other attributes 'ontology', 'lastUpdated', 'hasChildren', and 'hasParent'
-    return(matches)
+    return matches
     # # THE FOLLOWING IS OBSOLETE B/C: (1) we want to capture naked terms, and (2) this condition can be passed as a
     # #     parameter in the query string
     # # we only interested in non-naked terms (those which have definitions)
@@ -105,23 +119,34 @@ def resolve_term(term, ontologies=None):
     #         kept_matches.append(match)
     # return(kept_matches)
 
+
+# precondition: the argument is a string representation of an acronym used to identify an ontology
+# postcondition: the function will return a list in which the first element is the string representation of the name of
+#   the ontology from which the match was found and the second element is the date of the most recent update to the ontology
+def ontology_info(acronym):
+    ontology_query_string = REST_URL + '/ontologies/' + acronym + '/submissions'
+    ontology = get_json(ontology_query_string)[0]
+    my_ontology = {'name': ontology['ontology']['name'],
+                   'date': ontology['creationDate'][:10]}
+    return my_ontology
+
+
 # precondition: The first argument is a string representation of the searched term. The second argument is a list of
 #   strings, each string being a (CASE SENSITIVE) acronym associated with an ontology. This list of acronyms is the
 #   subset of ontologies to be included in the query.
 # postcondition: Constructs and returns a query string to be sent to BioPortal's API
-def query_string(term, ontologies=None):
-    if ontologies is None:
-        ontologies = []
+def term_query_string(term, ontologies):
     search_term = 'q=' + term
     ontologies_param = 'ontologies=' + ','.join(ontologies)
     my_query = '/search?' + search_term + '&' + ontologies_param
     return(my_query)
 
+
 # precondition: Argument is a string representation of the URL request to be made
 # postcondition: Returns a dictionary representation of the json response object
 def get_json(url):
-    myHeaders = {'Authorization':'apikey token='+API_KEY}
-    r = requests.get(url, headers=myHeaders)
+    my_headers = {'Authorization':'apikey token='+API_KEY}
+    r = requests.get(url, headers=my_headers)
     return(json.loads(r.text))
 
 
@@ -142,7 +167,61 @@ if __name__ == '__main__':
             inputFile = sys.argv[1]
             outputDirectory = os.path.dirname(inputFile)
         output_batch_query(inputFile, outputDirectory)
-        print("\nThe terms in %s have been resolved through BioPortal; Results have been stored in %s/resolved_terms.csv.\n"
+        print("\n\nThe terms in %s have been resolved through BioPortal.\nResults have been stored in %s/resolved_terms.csv.\n"
               % (inputFile, outputDirectory))
     else:
-        print("\nUSAGE: python batch-query.py <input textfile> <output directory>\n")
+        print("\n\nUSAGE: python batch-query.py <input textfile> <output directory>\n")
+
+
+# # -- TESTING --
+# children = []
+# i = 1
+# for link in links:
+#     print(i)
+#     time.sleep(.1)
+#     children.append(get_json(link['children']))
+#     i+=1
+# for child in children:
+#     print(child)
+#
+# eye = resolve_term('eye', [])
+# print(eye[0].keys())
+#
+# for match in eye:
+#     print(list(match.keys()))
+#
+# ALL_KEYS = ['searched_term','prefLabel', 'definition', 'synonym', 'ontology_acronym', 'ontology_name', 'last_updated', 'has_child', '@id']
+# eye_records = []
+# for match in eye:
+#     record = []
+#     for key in ALL_KEYS:
+#         record.append(match[key])
+#     eye_records.append(record)
+#
+# textfile_path = '/Users/timothy/Documents/ontology-batch-query/search-terms.txt'
+# directory = '/Users/timothy/Documents/ontology-batch-query/'
+# output_batch_query(inputPath = textfile_path,
+#                    outputPath = directory,
+#                    ontologies_param = None)
+#
+# debugging above call:
+# terms_file = open(textfile_path, 'r')
+# terms = []
+# for line in terms_file:
+#     terms.append(line.strip())
+# # use resolve_list() to resolve list of terms
+# results_list = resolve_list(terms, ontologies_param=[]) # list of lists each representing the matches of a term and
+# #   containing dictionaries of which each represent a single match
+# # creating output csv file
+# outfile = directory + '/resolved_terms.csv'
+# f = csv.writer(open(outfile, 'w'))
+# f.writerow(["searched_term", "matched_term", "definition", "synonyms", "ontology_acronym", "ontology_name",
+#            "last_updated", "has_child","URL"])
+# # populating csv with matches
+# for term_matches in results_list: # iterating through list of lists; each iteration is list of all matches for a term
+#     for match in term_matches: # iterating throguh list of dictionaries each representing a match
+#         record = []
+#         for key in ALL_KEYS:
+#             record.append(match[key])
+#         f.writerow(record)
+
